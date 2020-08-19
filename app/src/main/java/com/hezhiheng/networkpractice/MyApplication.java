8ce953678da85d4636eaa60ff6d250c89e990113
unknown <hzh_innovation@163.com>
2020-08-19 20:54:12 +0800

    [hzh] feat: add MyApplicaiton

---
+++ app/src/main/java/com/hezhiheng/networkpractice/MyApplication.java
@@ -0,0 +1,18 @@
+package com.hezhiheng.networkpractice;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MyApplication extends Application {
+    private static final int OPEN_TIMES_ZERO = 0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.count_open_times_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(getString(R.string.count_open_times_file), OPEN_TIMES_ZERO);
+        editor.apply();
+    }
+}
