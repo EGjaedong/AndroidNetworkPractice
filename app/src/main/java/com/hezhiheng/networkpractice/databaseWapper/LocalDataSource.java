c8c005bbcf5d529bd95b37df23ec8c3608a273dc
unknown <hzh_innovation@163.com>
2020-08-19 21:52:24 +0800

    [hzh] feat: add LocalDataSource

---
+++ app/src/main/java/com/hezhiheng/networkpractice/databaseWapper/LocalDataSource.java
@@ -0,0 +1,12 @@
+package com.hezhiheng.networkpractice.databaseWapper;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.hezhiheng.networkpractice.dao.PersonDao;
+import com.hezhiheng.networkpractice.entity.PersonEntity;
+
+@Database(entities = {PersonEntity.class}, version = 1)
+public abstract class LocalDataSource extends RoomDatabase {
+    public abstract PersonDao personDao();
+}
